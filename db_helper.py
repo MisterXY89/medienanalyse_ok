b8f51af6963ecce30839b158654468443a129e89
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-02-23 16:06:10 +0100

    store fix 20913

--- db_helper.py
+++ db_helper.py
@@ -4,18 +4,22 @@ import psycopg2
 from settings import POSTGRES_USER, POSTGRES_PASSWORD
 
 def write_url(story_id, media_id, title, url, collect_date, publish_date):
-    connection = psycopg2.connect(database="medienanalyse_ok",
-                        user=POSTGRES_USER,
-                        password=POSTGRES_PASSWORD,
-                        host="127.0.0.1",
-                        port="5432"
-                    )
-    cur = connection.cursor()
+    try:
+        connection = psycopg2.connect(database="medienanalyse_ok",
+                            user=POSTGRES_USER,
+                            password=POSTGRES_PASSWORD,
+                            host="127.0.0.1",
+                            port="5432"
+                        )
+        cur = connection.cursor()
 
-    cur.execute(f"INSERT INTO story_urls (story_id, media_id, title, url, collect_date, publish_date) \
-      VALUES ({story_id}, {media_id}, '{title}', '{url}', '{collect_date}', '{publish_date}');")
-    connection.commit()
-    connection.close()
+        cur.execute(f"INSERT INTO story_urls (story_id, media_id, title, url, collect_date, publish_date) \
+          VALUES ({story_id}, {media_id}, '{title}', '{url}', '{collect_date}', '{publish_date}');")
+        connection.commit()
+        connection.close()
+    except Exception as e:
+        print(e)
+        return False
 
 def write_text():
     pass
