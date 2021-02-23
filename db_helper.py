7388c0c947a286fcf513d648d0a1aeb18c262aa7
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-02-23 15:55:30 +0100

    conn fix

--- db_helper.py
+++ db_helper.py
@@ -4,18 +4,18 @@ import psycopg2
 from settings import POSTGRES_USER, POSTGRES_PASSWORD
 
 def write_url(story_id, media_id, title, url, collect_date, publish_date):
-    psycopg2.connect(database="medienanalyse_ok",
+    connection = psycopg2.connect(database="medienanalyse_ok",
                         user=POSTGRES_USER,
                         password=POSTGRES_PASSWORD,
                         host="127.0.0.1",
                         port="5432"
                     )
-    connection.cursor()
+    cur = connection.cursor()
 
     cur.execute(f"INSERT INTO story_urls (story_id, media_id, title, url, collect_date, publish_date) \
       VALUES ({story_id}, {media_id}, '{title}', '{url}', '{collect_date}', '{publish_date}');")
-    conn.commit()
-    conn.close()
+    connection.commit()
+    connection.close()
 
 def write_text():
     pass
