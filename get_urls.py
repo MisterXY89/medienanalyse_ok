4cab1fb7fe4aa34a3f0f51b4e7cf14f848f4f706
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-02-23 15:43:07 +0100

    instant write 2

--- get_urls.py
+++ get_urls.py
@@ -9,33 +9,6 @@ from settings import API_KEY, SEARCH_TERM, LANG, FROM_DATE, TO_DATE
 from db_helper import write_url, write_text
 from helper import url_filter
 
-
-
-mediacloud = mediacloud.api.MediaCloud(API_KEY)
-
-numOfStoriesTotal = int(mediacloud.storyCount(SEARCH_TERM, LANG, mediacloud.publish_date_query( datetime.date(2019, 1, 1), datetime.date(2019, 12, 31) ))["count"])
-print(numOfStoriesTotal)
-
-stories = []
-for dt in rrule.rrule(rrule.DAILY,
-					dtstart=datetime.datetime.strptime(FROM_DATE, '%Y-%m-%d'),
-					until=datetime.datetime.strptime(TO_DATE, '%Y-%m-%d')):
-	day = int(dt.strftime('%d'))
-	month = int(dt.strftime('%m'))
-	year = int(dt.strftime('%Y'))
-	date1 = datetime.datetime.strptime(f"{year}-{month}-{day}", "%Y-%m-%d")
-	print(f"Getting stories: for {date1}")
-	date2 = date1 + datetime.timedelta(days=1)
-	print(date2)
-    q_item = mediacloud.storyList(SEARCH_TERM, solr_filter=mediacloud.publish_date_query( date1, date2 ), rows = 100 )
-    store(q_item)
-	stories.append( q_item )
-
-
-
-stories = list(itertools.chain.from_iterable(stories))
-print(len(stories))
-
 def store(story):
 	url = story["url"]
 	print(url)
@@ -66,3 +39,28 @@ def store(story):
 	}
 
 	write_url(stories_id, media_id, title, url, collect_date, publish_date)
+
+mediacloud = mediacloud.api.MediaCloud(API_KEY)
+
+numOfStoriesTotal = int(mediacloud.storyCount(SEARCH_TERM, LANG, mediacloud.publish_date_query( datetime.date(2019, 1, 1), datetime.date(2019, 12, 31) ))["count"])
+print(numOfStoriesTotal)
+
+stories = []
+for dt in rrule.rrule(rrule.DAILY,
+					dtstart=datetime.datetime.strptime(FROM_DATE, '%Y-%m-%d'),
+					until=datetime.datetime.strptime(TO_DATE, '%Y-%m-%d')):
+	day = int(dt.strftime('%d'))
+	month = int(dt.strftime('%m'))
+	year = int(dt.strftime('%Y'))
+	date1 = datetime.datetime.strptime(f"{year}-{month}-{day}", "%Y-%m-%d")
+	print(f"Getting stories: for {date1}")
+	date2 = date1 + datetime.timedelta(days=1)
+	print(date2)
+    q_item = mediacloud.storyList(SEARCH_TERM, solr_filter=mediacloud.publish_date_query( date1, date2 ), rows = 100 )
+    store(q_item)
+	stories.append( q_item )
+
+
+
+stories = list(itertools.chain.from_iterable(stories))
+print(len(stories))
